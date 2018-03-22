aaea1ca684ecdfa4f18a97d6887931708b4cff59
Rafael Valdez <valdezr1@outlook.com>
2018-03-21 21:44:51 -0700

    Created Base Spring Boot Application

---
+++ BackEnd/src/main/java/io/hoth/birdie/Main.java
@@ -0,0 +1,14 @@
+package io.hoth.birdie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String args[]){
+        SpringApplication.run(Main.class, args);
+    }
+
+}
