a0a68029c3639cf611823ae5e90557815d1cb97e
NaveenJanarthanan <njanarthanan@spu.edu>
2018-04-27 05:23:11 -0700

    update

--- FrontEnd/birdie-react/src/Components/Profile/Profile.js
+++ FrontEnd/birdie-react/src/Components/Profile/Profile.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
 
 import NavBar from './../NavBar/NavBar';
+import CoinTable from './CoinTable/CoinTable';
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility,
+} from 'semantic-ui-react'
 
 class Profile extends Component {
   render() {
     return (
       <div>
         <NavBar/>
-        <h1>Hello Profile</h1>
+        <div className="righttable">
+        <CoinTable/>
+        </div>
+        <div className="usertext">
+        <h1>User</h1>
+        </div>
+        <div className="user">
+        <Image
+        src='/user.jpg'
+        size='medium'
+        />
+        </div>
       </div>
     )
   }
