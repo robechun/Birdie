7c0ec61eee8ce6e101c07cc3c8d95dadf410510d
Robert Chung <robertchung97@gmail.com>
2018-05-09 12:51:41 -0700

    partition to profile service

--- BackEnd/birdie-springboot/src/main/java/io/hoth/birdie/Controllers/ProfileController.java
+++ BackEnd/birdie-springboot/src/main/java/io/hoth/birdie/Controllers/ProfileController.java
@@ -1,11 +1,8 @@
 package io.hoth.birdie.Controllers;
 
-import io.hoth.birdie.DAO.UserRepository;
-import io.hoth.birdie.Entities.UserPrincipal;
+import io.hoth.birdie.Services.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -14,47 +11,21 @@ import org.springframework.web.bind.annotation.*;
 public class ProfileController {
 
     @Autowired
-    UserRepository userRepository;
+    ProfileService profileService;
 
-    // TODO: HAVE NOT BEEN TESTED!!
     @PostMapping(value = "/addApiKey")
     public ResponseEntity upsertApiKey(@RequestBody String apiKey) {
-        UserPrincipal currentUser = (UserPrincipal)
-                SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-
-        if (userRepository.upsertApiKey(currentUser.getUsername(), apiKey))
-            return new ResponseEntity(HttpStatus.OK);
-
-        return new ResponseEntity(HttpStatus.NOT_ACCEPTABLE);
-
+        return profileService.addApiKey(apiKey);
     }
 
-
-    // TODO: HAVE NOT BEEN TESTED!!
     @PostMapping(value = "/addSecret")
     public ResponseEntity upsertSecret(@RequestBody String secret) {
-        UserPrincipal currentUser = (UserPrincipal)
-                SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-
-        if (userRepository.upsertSecret(currentUser.getUsername(), secret))
-            return new ResponseEntity(HttpStatus.OK);
-
-        return new ResponseEntity(HttpStatus.NOT_ACCEPTABLE);
-
+        return profileService.addSecret(secret);
     }
 
-    // TODO: HAVE NOT BEEN TESTED!!
     @PutMapping(value = "/newPassword")
     public ResponseEntity changePassword(@RequestBody String password) {
-        UserPrincipal currentUser = (UserPrincipal)
-                SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        if (!userRepository.setPassword(currentUser.getUsername(), password))
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-
-        return new ResponseEntity(HttpStatus.OK);
-
+        return profileService.newPassword(password);
     }
 }
