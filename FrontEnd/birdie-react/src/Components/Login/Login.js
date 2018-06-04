f45a5e0499c11bf1aea312bc98c35fff27cf020c
Rafael Valdez <valdezr1@outlook.com>
2018-06-04 01:26:39 -0700

    login page footer component used and signup button working

--- FrontEnd/birdie-react/src/Components/Login/Login.js
+++ FrontEnd/birdie-react/src/Components/Login/Login.js
@@ -5,7 +5,8 @@ import {connect} from 'react-redux';
 import {newToken} from "../../Actions/loginActions";
 
 import {Redirect} from 'react-router-dom'
-import {Button, Grid, Header, Form, Image, Message, Segment, Container, List} from 'semantic-ui-react'
+import {Button, Grid, Header, Form, Image, Message, Segment} from 'semantic-ui-react'
+import Footer from "../Footer/Footer";
 
 
 class LoginForm extends Component {
@@ -48,13 +49,6 @@ class LoginForm extends Component {
     }
 
     render() {
-        // let redirect = <div/>
-        // if(this.state.redirect == true) {
-        //     console.log("Log-in successful!");
-        //     redirect = <Redirect to={{pathname : '/', state : {accessToken : this.state.accessToken, redirect : true}}} />
-        // }
-
-
         return (
             <div className="full blackout">
                 <div className='registerForm-form'>
@@ -100,41 +94,13 @@ class LoginForm extends Component {
                                     <Button color='black' type='button' fluid size='large' onClick={this.handleLogin}>Login</Button>
                                 </Segment>
                             </Form>
-                            <Message>
-                                New? <a href="/Register">Sign Up</a>
-                            </Message>
+                            <br/>
+                            <Button size="large" href="/register"> New? Sign-Up! </Button>
                         </Grid.Column>
                     </Grid>
 
                 </div>
-
-            <Segment inverted vertical style={{ padding: '5em 0em' }}>
-            <Container>
-                <Grid divided inverted stackable>
-                    <Grid.Row>
-                        <Grid.Column width={3}>
-                            <Header inverted as='h4' content='About' />
-                            <List link inverted>
-                                <List.Item as='a'>Sitemap</List.Item>
-                                <List.Item as='a'>Contact Us</List.Item>
-                            </List>
-                        </Grid.Column>
-                        <Grid.Column width={3}>
-                            <Header inverted as='h4' content='Learn' />
-                            <List link inverted>
-                                <List.Item as='a'>Binance</List.Item>
-                                <List.Item as='a'>Investing in Crypto</List.Item>
-                                <List.Item as='a'>How To Access</List.Item>
-                            </List>
-                        </Grid.Column>
-                        <Grid.Column width={7}>
-                            <Header as='h4' inverted>Birdie</Header>
-                            <p>Secure your financial future. Create an account now.</p>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </Container>
-        </Segment>
+                <Footer/>
             </div>
         )
     }
